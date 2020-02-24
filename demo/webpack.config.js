5eb3543cf2e551383a3d1abff7a75be3b1761a06
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-02-24 10:32:59 +0900

    Fix typescript config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,19 +2,31 @@ var path = require('path');
 var webpack = require('webpack')
 
 module.exports = {
-    cache: true,
-    entry: './demo.js',
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'demo.build.js',
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
-    plugins: []
-};
+  cache: true,
+  entry: './demo.js',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'demo.build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'ts-loader'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx']
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,41 @@
-var path = require('path');
-var webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
-    entry: {
-        index: [
-            './index.js'
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'microcharts.js',
+    library: 'ReactMicrocharts',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'ts-loader'
         ]
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        publicPath: '/',
-        filename: '[name].js',
-        library: 'ReactMicrocharts',
-        libraryTarget: 'umd'
-    },
-    externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        }
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
-};
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx']
+  }
+}
