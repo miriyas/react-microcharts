e2c416775b9b5d5e324da74e72eee7c6bf008617
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-05-01 21:21:59 +0900

    Update typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,42 @@ var path = require('path');
 var webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        index: [
-            './index.js'
-        ]
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        publicPath: '/',
-        filename: '[name].js',
-        library: 'ReactMicrocharts',
-        libraryTarget: 'umd'
-    },
-    externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        }
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
+  entry: {
+    index: [
+      './index.js'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: '[name].js',
+    library: 'ReactMicrocharts',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
+  resolve: {
+    extensions: ['.tsx', '.jsx', '.json']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }],
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development'
 };
